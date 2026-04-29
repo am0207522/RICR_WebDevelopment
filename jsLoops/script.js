dea08695d01c1b74ed07f4b97174cb69ab15d3e4
am0207522 <am0207522@gmail.com>
2026-04-29 23:55:05 +0530

    js_loop

---
+++ jsLoops/script.js
@@ -0,0 +1,28 @@
+// for(var i=1; i <= 10; i++){
+//     console.log(i*4);
+// }
+
+let i=1;
+ while (i <= 10 );{
+    i++;
+ console.log(i*2);
+    i++;
+ }
+
+
+
+
+
+
+
+
+
+
+
+// let array = [24,58,33,86,96,12,53,23,32,100];
+
+// for(let i=0; i<10; i++);{
+//     console.log(a[i]);
+//     i++;
+// }
+

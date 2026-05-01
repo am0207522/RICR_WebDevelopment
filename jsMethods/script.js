1feaf62638e1fc472dd369352d8c671b7556c29c
am0207522 <am0207522@gmail.com>
2026-05-01 19:51:09 +0530

    Function_js

---
+++ jsMethods/script.js
@@ -0,0 +1,37 @@
+// topic fuction
+
+// let a = 20, b=30;
+// function sum (x,y){
+// let z = x+y
+// console.log(z);
+// }
+// sum(a,b)
+
+// <- topic FUNCTION EXPRESSON ->
+// let xyz = function () {
+//   console.log("Hellor World");
+// };
+// console.log(xyz);
+// callback functionn
+// OR
+// xyz();
+// Normal Function call
+
+// topic 00 example
+// let add = function (a, b) {
+//   return a + b;
+// };
+// console.log(add);
+// console.log(add(4, 6));
+
+
+// topic arrow functionn.
+// const arrowfun = () => {
+//   console.log("i am arrow function");
+// };
+// arrowfun();
+
+// or function direct call
+const  addarrow = (a,b) => a+b;
+console.log(addarrow(5,6));
+

24e6cb80304b9a4aeff25bec046b408c74d8e292
am0207522 <am0207522@gmail.com>
2026-06-26 23:44:10 +0530

    style: Improve code comments and formatting for clarity

--- Backend/FirstBackend/index.js
+++ Backend/FirstBackend/index.js
@@ -1,35 +1,35 @@
-import dotenv from "dotenv";
+import dotenv from "dotenv"; //environment variable load
 dotenv.config();
 
-import express from "express";
+import express from "express"; // express
 import AuthRouter from "./src/routers/auth.route.js";
 import PublicRouter from "./src/routers/public.route.js";
 import connectDB from "./src/config/dbConnection.config.js";
 
-const app = express();
+const app = express(); // object
 
-app.use(express.json());
+app.use(express.json()); // middleware
 
 app.use("/auth", AuthRouter);
 app.use("/public", PublicRouter);
 
-//Default API
+// default API
 app.get("/", (req, res) => {
   console.log("Default Get API Hit");
-  res.json({ message: "Welcome to my first backend Project" });
+  res.json({ message: "Welcome to my first backend Project" }); // api creations
 });
 
-//Default Error Handler
+// Default Error Handler
 app.use((err, req, res, next) => {
-  const ErrMessage = err.message || "Internal Server Error";
-  const ErrStatusCode = err.statusCode || 500;
+  const ErrorMessage = err.message || "Internal Server Error";
+  const ErrorStatusCode = err.statusCode || 500;
 
-  res.status(ErrStatusCode).json({ message: ErrMessage });
+  res.status(ErrorStatusCode).json({ message: ErrorMessage });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; // verify the port
 
 app.listen(port, () => {
-  console.log("Server Started on port:", port);
+  console.log("Server Strated on PORT:", port); //
   connectDB();
 });
